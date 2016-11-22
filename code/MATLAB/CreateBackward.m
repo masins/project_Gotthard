ccc9b14cf91e4e71eb0001f7303f2caba208bc74
pensara <Sara@public-docking-hpx-0146.ethz.ch>
2016-11-22 09:26:08 +0100

    Aggiunto errori a create...

--- code/MATLAB/CreateBackward.m
+++ code/MATLAB/CreateBackward.m
@@ -1,4 +1,19 @@
+%
+%INPUT:     a line with only 0 and -1
+%
+%OUTPUT:    The same line with a car at the end with moving left (-1) if the
+%           last place is free
+%
+%ERROR:     If the line contai numbers that are not 0 or -1
+
 function y = CreateBackward(X)
+
+for i = 1:1:length(X)
+    if((X(1,i) ~= 0) && (X(1,i)~= -1))
+        error('Error. \nThe move backward line have a wrong value at (1,%d)',i);
+    end
+end
+
 if X(1,length(X))==0
     X(1,length(X))=-1;
 end
