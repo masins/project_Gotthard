780858ed9a528e08780cea4d56d85167e2784493
Enea Masina <ene1@hotmail.it>
2016-11-30 09:59:06 +0100

    test semaforo funzione con sensori + test conto flusso

--- code/MATLAB/test_semaforo.m
+++ code/MATLAB/test_semaforo.m
@@ -1,14 +1,33 @@
-%INPUT: L: left line, R: right line, C: center line, t: timer, 
-%tg: time green, ttr: time red
+%INPUT: L: left line, R: right line, C: center line, t: timer, tg: time green, ttr: time red
 function [nL,nR,nC,nt] = test_semaforo(L,R,C,t,tg,tr)
-
 nL = L;
 nR = R;
 nC = C;
 nt = t;
 ttot = 2*tg+2*tr;
 
-%SEMAFORO IN SE
+%Sensore a sinistra e a destra in caso t=0
+if(nt==0)
+    if(WaitCarPresence(nL) == 1)
+        nt=1;
+    elseif(WaitCarPresence(nR) == -1)
+        nt= tg+tr+1;
+    end
+end
+
+%sensore a sinistra e a destra in caso t= tg+tr
+if(nt== tg+tr)
+    if(WaitCarPresence(nR) == -1)
+    elseif(WaitCarPresence(nR) == 1)
+        nt=1;
+    elseif(WaitCarPresence(nR) == 0)
+        nt=0;
+    else
+        error('qualcosa non funziona nel sensore a timer = %d',nt);
+    end
+end
+
+%SEMAFORO verde a sinistra o a destra
 if(mod(t,ttot) > 0 && mod(t,ttot) <= tg)
     if(nL(1,50) == 1)
         nC = CreateForward(nC);
@@ -21,11 +40,16 @@ elseif((mod(t,ttot)>(tg+tr)) && (mod(t,ttot)<=(2*tg+tr)))
     end
 end
 
+%fai avanzare o resetta timer
+%Errore se timer prende valore non concesso
 if nt == ttot
-    nt = 1;
-elseif nt < ttot
-    nt = nt +1;
+    nt = 0;
+elseif ((nt>0) && (nt < ttot))
+    nt = nt+1;
+elseif(nt==0)
+    nt=0;
 else
-    error('Error. The timer takes wrong value \ntimer = %d',t);
+    error('Error. The timer takes wrong value \ntimer = %d',nt);
+end
 
 end
