717f45432a6071b440c5bd5b47586cfbfd928c80
pensara <Sara@public-docking-etx-2312.ethz.ch>
2016-11-30 11:16:35 +0100

    test semaforo migliorato

--- code/MATLAB/test_semaforo_main.m
+++ code/MATLAB/test_semaforo_main.m
@@ -16,52 +16,57 @@ fluxB = zeros(1,24);
 h=1;
 
 for t = 1 : 1 : 86400
-%Crea auto
-A = CreateForward(A);
-C = CreateBackward(C);
-
-%muovi auto
-A = MoveForward(A);
-B = MoveForward(B);
-C = MoveBackward(C);
-D = MoveBackward(D);
-X = MoveForward(X);
-X = MoveBackward(X);
-
-%Svuota la line centrale in automatico
-if X(1,10) == 1
-    B = CreateForward(B);
-    X(1,10) = 0;
-elseif X(1,1)==-1
-    D = CreateBackward(D);
-    X(1,1)=0;
-end
-
-%funzione semaforo
-[A,C,X,timer] = test_semaforo(A,C,X,timer,tg,tr);
-
-%grafici
-if D(1,1)==0
-    TD(1,t)=0;
-elseif D(1,1)==-1
-    TD(1,t) = -1;
-    D(1,1)=0;
-    car_counterD = car_counterD + 1;
-end
-if B(1,50)==0
-    TB(1,t)=0;
-elseif B(1,50)==1
-    TB(1,t) = 1;
-    B(1,50)=0;
-    car_counterB = car_counterB + 1;
-end
-
-if mod(t,3600) == 0 && h < 25
-    fluxD(1,h)=car_counterD;
-    fluxB(1,h)=car_counterB;
-    h = h + 1;
-    car_counterD = 0;
-    car_counterB = 0;
-end  
-
+    
+    % create car
+    A = CreateForward(A);
+    C = CreateBackward(C);
+    
+    % move car
+    A = MoveForward(A);
+    B = MoveForward(B);
+    C = MoveBackward(C);
+    D = MoveBackward(D);
+    X = MoveForward(X);
+    X = MoveBackward(X);
+    
+    % free center line if needed
+    if X(1,10) == 1
+        B = CreateForward(B);
+        X(1,10) = 0;
+    elseif X(1,1)==-1
+        D = CreateBackward(D);
+        X(1,1)=0;
+    end
+    
+    % traffic light function
+    [A,C,X,timer] = test_semaforo(A,C,X,timer,tg,tr);
+    
+    % graphs
+    % TD is 1 if a car passes through D and 0 otherwise
+    % TB is 1 if a car passes through B and 0 otherwise
+    if D(1,1)==0
+        TD(1,t)=0;
+    elseif D(1,1)==-1
+        TD(1,t) = -1;
+        D(1,1)=0;
+        car_counterD = car_counterD + 1;
+    end
+    if B(1,50)==0
+        TB(1,t)=0;
+    elseif B(1,50)==1
+        TB(1,t) = 1;
+        B(1,50)=0;
+        car_counterB = car_counterB + 1;
+    end
+    
+    % fluxD represents the number of cars passing through D in one hour
+    % FluxB represents the number of cars passing through B in one hour
+    if mod(t,3600) == 0 && h < 25
+        fluxD(1,h)=car_counterD;
+        fluxB(1,h)=car_counterB;
+        h = h + 1;
+        car_counterD = 0;
+        car_counterB = 0;
+    end
+    
 end
