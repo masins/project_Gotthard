a92216232a3be431206d40d2f70c6d4970016669
Enea Masina <ene1@hotmail.it>
2016-12-14 17:23:58 +0100

    Commento randomgen

--- code/MATLAB/RandGen.m
+++ code/MATLAB/RandGen.m
@@ -1,23 +1,32 @@
+%make the random generation pro houar
+%Input: A matrix with the number of car for each hour(colon) and each
+%       day(row)
+%
+%Output: A vector with 0 and 1 rnadon corrispondig at car cration in an hour 
 
 function Y =  RandGen(R)
 Y = zeros(24*3,3600);
 
 for d=0 : 1 :2
     for h=1:1:24
-
+        
+        %selection spot with the number of car
         tot = 3600;
         N = R(d+1,h);
         M = tot - N;
 
+        %the vector of that hour with only 1(create car) and 0(not crate)
         X = [zeros(1,M), ones(1,N)];
-
+        
+        %randomising
         X = X(randperm(tot));
 
+        %prepering random matrix
         Y(24*d+h,:)=X;
     end
 end
 
-%sezione di prova mettere tutta la matrice su una sola linea
+%Matrix in vectot trasformation
 Y = Y';
 Y= Y(:)';
 
